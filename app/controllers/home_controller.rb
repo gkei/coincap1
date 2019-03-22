25c1307fbd8af00799af361f0f4ca2386afc0da6
gkey <lemino@gmail.com>
2019-03-22 17:48:47 +0000

    integrated pnomics api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
   def index
-    @hello = 'kom kyk saam'
+    
+    require 'net/http'
+    require 'json'
+    
+    @url    = 'https://api.nomics.com/v1/currencies/interval?key=c96481e6aae21afba764a5e8e3fd6c34&start=2019-03-22T00%3A00%3A00Z&end=2019-03-22T00%3A00%3A05Z'
+    @uri    = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins  = JSON.parse(@response)
+    
+    @mycoins= ['BTC','LTC','XRP','NCASH','XLM',]
   end
   
   def about
