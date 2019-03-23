4f1824ad2244df2f915a18f1b0862f9fbc0671b7
gkey <lemino@gmail.com>
2019-03-23 20:21:36 +0000

    added second API lookup & installed devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,9 +10,29 @@ class HomeController < ApplicationController
     @coins  = JSON.parse(@response)
     
     @mycoins= ['BTC','LTC','XRP','NCASH','XLM',]
+    
   end
   
   def about
   end
   
+  def lookup
+    @currency = params[:Cur]
+    
+    if @currency                        # if it exists
+        @currency = @currency.upcase
+    end
+    
+    if @currency == ''                  # if if is empty
+        @currency = 'Het jy vergeet om n currency in te tik?'
+    end
+    
+      @url    = 'https://api.nomics.com/v1/markets/prices?key=c96481e6aae21afba764a5e8e3fd6c34&currency=BTC' #+ @currency
+      @uri    = URI(@url)
+      @response = Net::HTTP.get(@uri)
+      @onecurrency  = JSON.parse(@response)
+    
+  end
+  
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#index'
   get 'home/about'
+  get 'home/lookup'
+  post '/home/lookup' => 'home/lookup'
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
